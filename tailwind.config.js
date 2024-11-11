447c10bc9299203cee6de2588b184cfa3dcf9f0d
F0oocuS <ihoryaremko@gmail.com>
2024-11-11 23:06:21 +0200

    add mobile styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,13 @@ module.exports = {
 			'roboto': 'Roboto, sans-serif',
 			'roboto-mono': 'Roboto mono, sans-serif'
 		},
-		screens: {
-			xl: { max: "1279px" },
-			lg: { max: "1023px" },
-			md: { max: "767px" },
-			sm: { max: "639px" }
-		}
+		// screens: {
+			// 'sm': {'min': '640px', 'max': '767px'},
+			// 'md': {'min': '768px', 'max': '1023px'},
+			// 'lg': {'min': '1024px', 'max': '1279px'},
+			// 'xl': {'min': '1280px', 'max': '1535px'},
+			// '2xl': {'min': '1536px'},
+		// }
 	},
 	plugins: [],
 }
